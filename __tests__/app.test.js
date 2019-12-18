7876f65d5ae81f2aeb8df7ca774de8a3b07b0e78
Jodi N Kansagor <joedvice@gmail.com>
2019-12-18 13:59:45 -0800

    get and get by id routes

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -59,4 +59,38 @@ describe('app routes', () => {
       });
   });
 
+  it('gets a nirvana show by Id', () => {
+    return request(app)
+      .get(`/api/v1/nirvanas/${nirvana.id}`)
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: nirvana.id,
+          date: date.toISOString(),
+          venue: 'City Gardens',
+          city: 'Trenton',
+          state: 'New Jersey',
+          country: 'United States',
+          recorded: 'true',
+          __v: 0
+        });
+      });
+  });
+
+  it('gets all nirvana shows', () => {
+    return request(app)
+      .get('/api/v1/nirvanas')
+      .then(res => {
+        expect(res.body).toEqual([{
+          _id: nirvana.id,
+          date: date.toISOString(),
+          venue: 'City Gardens',
+          city: 'Trenton',
+          state: 'New Jersey',
+          country: 'United States',
+          recorded: 'true',
+          __v: 0
+        }]);
+      });
+  });
+
 });
