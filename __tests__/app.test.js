e7f1650b22330c045dae52b86d7e52657c72bbac
Jodi N Kansagor <joedvice@gmail.com>
2019-12-18 13:55:03 -0800

    post route

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,6 +4,7 @@ const request = require('supertest');
 const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
+const Nirvana = require('../lib/models/Nirvana')
 
 describe('app routes', () => {
   beforeAll(() => {
@@ -14,7 +15,48 @@ describe('app routes', () => {
     return mongoose.connection.dropDatabase();
   });
 
+  let nirvana;
+  let date;
+  beforeEach(async () => {
+    date = new Date();
+    nirvana = await Nirvana.create({
+      date,
+      venue: 'City Gardens',
+      city: 'Trenton',
+      state: 'New Jersey',
+      country: 'United States',
+      recorded: 'true'
+    });
+  });
+
   afterAll(() => {
     return mongoose.connection.close();
   });
+
+  it('can create a new nirvana', () => {
+    const date = new Date();
+    return request(app)
+      .post('/api/v1/nirvanas')
+      .send({
+        date,
+        venue: 'City Gardens',
+        city: 'Trenton',
+        state: 'New Jersey',
+        country: 'United States',
+        recorded: 'true'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          date: date.toISOString(),
+          venue: 'City Gardens',
+          city: 'Trenton',
+          state: 'New Jersey',
+          country: 'United States',
+          recorded: 'true',
+          __v: 0
+        });
+      });
+  });
+
 });
